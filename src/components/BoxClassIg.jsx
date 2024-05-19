5c5951f3439d50f93d2f10e25b46836ab6581c24
Wijaya Dev <118777889+widev71@users.noreply.github.com>
2024-05-19 10:31:07 +0800

    Update BoxClassIg.jsx

--- src/components/BoxClassIg.jsx
+++ src/components/BoxClassIg.jsx
@@ -6,7 +6,7 @@ const BoxClassIg = () => {
 					<img src="/Instagram.svg" alt="" className="w-auto h-10" />
 					<img src="/next.png" alt="" className="h-4 w-4" />
 				</div>
-					<h1 className="capitalize text-white text-left pr-5 text-base font-semibold  mt-5">Text Anonim
+					<h1 className="capitalize text-white text-left pr-5 text-base font-semibold  mt-5">Instagram
 				</h1>
 			</a>
 		</div>
