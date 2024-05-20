a6fe0e12f624b8252dab8fb6147a7eba520a1b07
Wijaya Dev <118777889+widev71@users.noreply.github.com>
2024-05-20 13:24:12 +0800

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -1,13 +1,25 @@
 const Senin = () => {
     return (
         <>
-        	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Sejarah</div>
-					<div className="flex justify-center items-center text-sm">07:30</div>
+        		<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">SEJARAH</div>
+					<div className="flex justify-center items-center text-sm">07.30-10.22</div>
 				</div>
-				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">Desain Grafis</div>
-					<div className="flex justify-center items-center text-sm">10.30-15.40</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">10.22-11.02</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Desain Grafis</div>
+					<div className="flex justify-center items-center text-sm">11.02-13.11</div>
+				</div>
+				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
+					<div className="w-[50%] text-base  font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">13.11-13.31</div>
+				</div>
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+					<div className="w-[50%] text-base  font-medium">Desain Grafis</div>
+					<div className="flex justify-center items-center text-sm">13.31-15.40</div>
 				</div>
         </>
     )
