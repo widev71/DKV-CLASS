8fa5fc9bbc7c8594ea4748f3c1dceffdbedd35ef
Wijaya Dev <118777889+widev71@users.noreply.github.com>
2024-05-20 13:31:28 +0800

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -1,6 +1,6 @@
 const Selasa = () => {
     return (
-        <>
+        <div>
         		<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
 					<div className="w-[50%] text-base  font-medium">Desain Grafis/div>
 					<div className="flex justify-center items-center text-sm">07.30-10.22</div>
@@ -21,7 +21,7 @@ const Selasa = () => {
 					<div className="w-[50%] text-base  font-medium">Desain Grafis</div>
 					<div className="flex justify-center items-center text-sm">13.31-15.40</div>
 				</div>
-        </>
+        </div>
     )
 } 
 
