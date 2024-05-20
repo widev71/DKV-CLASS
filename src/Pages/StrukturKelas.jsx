a5c8b5132fe529a46a7a46470c0fac3981920bf0
Wijaya Dev <118777889+widev71@users.noreply.github.com>
2024-05-20 12:24:03 +0800

    Update StrukturKelas.jsx

--- src/Pages/StrukturKelas.jsx
+++ src/Pages/StrukturKelas.jsx
@@ -282,9 +282,10 @@ const StrukturKelas = () => {
 						<img src="LineVertikal3.svg" alt="" />
 						<img src="Circle.svg" alt="" className="relative top-[-0.7rem]" />
 						<div className="relative bottom-3">
-							<BorderStruktur Jabatan="KETUA TEFA OCEAN" Nama="DEDI PERDANA" Width="120px" />
+							<BorderStruktur Jabatan="" Nama="NGAKAN NYOMAN" Width="120px" />
 							<div className="py-[3%]"></div>
-							<BorderStruktur Jabatan="WAKIL TEFA OCEAN" Nama="BAYU SADEWA" Width="120px" />
+							<BorderStruktur Jabatan="" Nama="SANI" Width="120px" />
+							<BorderStruktur Jabatan="" Nama="DIAH" Width="120px" />
 						</div>
 					</div>
 				</div> }
