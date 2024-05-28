4e25c8dd93fd0b503cfb8c768562210f7d541c87
Ducky <118777889+widev71@users.noreply.github.com>
2024-05-28 09:26:44 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore"
-
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDaZGFr2ncYlf-evyTWGmfhYuv1XevKo9g",
-  authDomain: "dkv-class.firebaseapp.com",
-  projectId: "dkv-class",
-  storageBucket: "dkv-class.appspot.com",
-  messagingSenderId: "829314916015",
-  appId: "1:829314916015:web:068bb08bd72ff146dc7e5a",
-  measurementId: "G-EYGM1R0NW6"
+  apiKey: "AIzaSyC_91x8Q6f1l7COAbGgQO8BwElbEBN6Vrk",
+  authDomain: "xi-dkv.firebaseapp.com",
+  projectId: "xi-dkv",
+  storageBucket: "xi-dkv.appspot.com",
+  messagingSenderId: "630354977430",
+  appId: "1:630354977430:web:5d7553c97390fe56e358b7",
+  measurementId: "G-TV32K9M01S"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
